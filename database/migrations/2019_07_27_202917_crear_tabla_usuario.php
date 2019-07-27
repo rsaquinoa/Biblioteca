ee89b51400acc2a6582cd9e2f18c061303915c14
rsaquinoa <rsaquinoa@gmail.com>
2019-07-27 18:42:52 -0400

    Creacion incial de tablas

---
+++ database/migrations/2019_07_27_202917_crear_tabla_usuario.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaUsuario extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->BigIncrements('id');
+            $table->string('usuarios',50);
+            $table->string('password',100);
+            $table->string('nombre',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Usuarios');
+    }
+}
