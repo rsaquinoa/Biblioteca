c7fe1a78d7b772d993eb373f86b961a36809a6ac
rsaquinoa <rsaquinoa@gmail.com>
2019-08-03 18:20:17 -0400

    Creacion de Munus y validacion de datos de entrada

--- resources/views/admin/menu/form.blade.php
+++ resources/views/admin/menu/form.blade.php
@@ -1,13 +1,13 @@
 <div class="form-group">
     <label for="nombre" class="col-lg-3 control-label requerido">Nombre</label>
     <div class="col-lg-8">
-        <input type="text" name="nombre" id="nombre" class="form-control" value="{{old("nombre")}}" required/>
+        <input type="text" name="nombre" id="nombre" class="form-control" value="{{old("nombre")}}"/>
     </div>
 </div>
 <div class="form-group">
     <label for="url" class="col-lg-3 control-label requerido">Url</label>
     <div class="col-lg-8">
-        <input type="text" name="url" id="url" class="form-control" value="{{old("url")}}" required/>
+        <input type="text" name="url" id="url" class="form-control" value="{{old("url")}}"/>
     </div>
 </div>
 <div class="form-group">
@@ -18,4 +18,4 @@
     <div class="col-lg-1">
         <span id="mostrar-icono" class="fa fa-fw {{old("icono")}}"><span>
     </div>
-</div>
+</div> 
