e2c0da5c8a99e9c9f2f8db40e51e106ac080b467
rsaquinoa <rsaquinoa@gmail.com>
2019-08-07 18:27:22 -0400

    Correcciones ordenar menu listo

--- public/assets/pages/scripts/admin/menu/index.js
+++ public/assets/pages/scripts/admin/menu/index.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-    $('#netstable').nestable().on('change', function (){
+$(document).ready(function () {
+    $('#nestable').nestable().on('change', function () {
         const data = {
             menu: window.JSON.stringify($('#nestable').nestable('serialize')),
             _token: $('input[name=_token]').val()
         };
         $.ajax({
-            url: '',
+            url: '/admin/menu/guardar-orden',
             type: 'POST',
             dataType: 'JSON',
             data: data,
-            success: function(respuesta){
+            success: function (respuesta) {
             }
         });
     });
